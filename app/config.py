07aca50a56d8f330b66566b232a1e3c45ee92f12
matolgithub <89175955650@mail.ru>
2022-11-27 20:28:29 +0300

    Next commit: new files - app.py, auth.py, config.py, example-requests.http, add in requirements.txt, change code in models.py, docker-compose.yml.

--- app/config.py
+++ app/config.py
@@ -1,1 +1,13 @@
+from os import getenv
+from kombu import Queue, Exchange
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+task_queues = (
+    Queue('default', Exchange('default'), routing_key='default'),
+)
+
+backend_redis = getenv('BACKEND_REDIS')
+broker_redis = getenv('BROKER_REDIS')
